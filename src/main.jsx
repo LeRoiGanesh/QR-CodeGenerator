9ef691f69b85838477540312f7b72ae616ae1a1b
LeRoi Ganesh <vente@Ganeshs-MacBook-Air.local>
2025-08-21 20:51:43 +0200

    Ajout de vercel.json pour la gestion des routes SPA et routes de fallback

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import QRPage from './pages/qr';
 
@@ -14,6 +14,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<LandingPage />} />
         <Route path="/qr" element={<QRPage />} />
         <Route path="/Dashboard" element={<App />} />
+        <Route path="/dashboard" element={<Navigate to="/Dashboard" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
